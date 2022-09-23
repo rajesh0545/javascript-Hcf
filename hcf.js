83be918e7d9c8f2f2e72e7f2d547a637f5bfb08f
rajesh <krajeshgowri7032@mail.com>
2022-09-23 14:50:24 +0530

    add files

---
+++ hcf.js
@@ -0,0 +1,15 @@
+ const number1=60;
+const number2=38;
+let hcf;
+for(i=1;i<=number1 &&  number2;i++){
+    if(number1 % i==0 && number2 % i ==0){
+        hcf=i;
+
+    }
+ 
+}
+
+ console.log(`hcf of ${number1} and ${number2} is ${hcf}`)
+
+
+
